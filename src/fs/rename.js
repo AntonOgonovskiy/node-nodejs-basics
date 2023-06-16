9908dec0eb045151ec31b1698d498f6b8e9f4b02
Anton <antonogonovskiy@gmail.com>
2023-06-16 21:29:00 +0300

    feat: implement rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
-const rename = async () => {
-    // Write your code here 
-};
+import { access } from "node:fs/promises";
+import { rename as changeName } from "node:fs/promises";
+try {
+  await access("src/fs/files/wrongFilename.txt");
+  await changeName(
+    "src/fs/files/wrongFilename.txt",
+    "src/fs/files/properFilename.md"
+  );
+} catch (err) {
+  console.log(err.message);
+  console.log("FS operation failed");
+}
+const rename = async () => {};
 
 await rename();
