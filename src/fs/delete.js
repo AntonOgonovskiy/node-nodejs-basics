9881a5016b585ed43c91fe1be7e6fd83812cc15c
Anton <antonogonovskiy@gmail.com>
2023-06-16 21:40:07 +0300

    feat: implement delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import { access } from "node:fs/promises";
+import { rm } from "node:fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await access("src/fs/files/fileToRemove.txt");
+    rm("src/fs/files/fileToRemove.txt");
+  } catch (err) {
+    console.log(err.message);
+    console.log("FS operation failed");
+  }
 };
 
 await remove();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,15 +1,17 @@
 import { access } from "node:fs/promises";
 import { rename as changeName } from "node:fs/promises";
-try {
-  await access("src/fs/files/wrongFilename.txt");
-  await changeName(
-    "src/fs/files/wrongFilename.txt",
-    "src/fs/files/properFilename.md"
-  );
-} catch (err) {
-  console.log(err.message);
-  console.log("FS operation failed");
-}
-const rename = async () => {};
+
+const rename = async () => {
+  try {
+    await access("src/fs/files/wrongFilename.txt");
+    await changeName(
+      "src/fs/files/wrongFilename.txt",
+      "src/fs/files/properFilename.md"
+    );
+  } catch (err) {
+    console.log(err.message);
+    console.log("FS operation failed");
+  }
+};
 
 await rename();
