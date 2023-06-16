c2bac0af5e911d30d2a8ebbafa759396449cff2a
Anton <antonogonovskiy@gmail.com>
2023-06-16 19:34:12 +0300

    feat: implement create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { writeFile } from "node:fs/promises";
+import { Buffer } from "node:buffer";
+import { access } from "node:fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  const data = new Uint8Array(Buffer.from("I am fresh and young"));
+  try {
+    await access("src/fs/files/fresh.txt");
+    console.log("FS operation failed");
+  } catch (err) {
+    if (err) {
+      const promise = writeFile("src/fs/files/fresh.txt", data);
+      await promise;
+    }
+  }
 };
 
 await create();
