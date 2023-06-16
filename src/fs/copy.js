27687e47e258bb8422e7ab61b205254dfad62ac8
Anton <antonogonovskiy@gmail.com>
2023-06-16 21:21:01 +0300

    feat: implement copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import { mkdir } from "node:fs/promises";
+import { access } from "node:fs/promises";
+import { cp } from "node:fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await access("src/fs/files");
+    const projectFolder = new URL("files_copy", import.meta.url);
+    await mkdir(projectFolder);
+    await cp("src/fs/files/", "src/fs/files_copy", { recursive: true });
+  } catch (err) {
+    console.log(err.message);
+    console.log("FS operation failed");
+  }
 };
 
 await copy();
