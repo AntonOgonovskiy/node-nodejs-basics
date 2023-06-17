dda0cc585e12645b537613078c2f8e9c2bf0fa4d
Anton <antonogonovskiy@gmail.com>
2023-06-17 17:37:23 +0300

    feat:implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import { createHash } from "crypto";
+import { readFile } from "node:fs/promises";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const content = await readFile("src/hash/files/fileToCalculateHashFor.txt", {
+    encoding: "utf8",
+  });
+  const hash = createHash("sha256").update(content).digest("hex");
+  console.log(hash);
 };
 
 await calculateHash();
