408274e9dcaa20668fc82371c651e668ef45e0dd
Anton <antonogonovskiy@gmail.com>
2023-06-17 22:44:07 +0300

    feat: implement write stream task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,9 @@
+import { createWriteStream } from "fs";
+import { pipeline } from "stream/promises";
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream("src/streams/files/fileToWrite.txt");
+  await pipeline(process.stdin, stream);
 };
 
 await write();
