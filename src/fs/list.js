48aee7ac13d4580e0dc981c0424d7d4c015cf8d7
Anton <antonogonovskiy@gmail.com>
2023-06-17 16:31:15 +0300

    feat: implement list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { opendir } from "node:fs/promises";
+import { access } from "node:fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    await access("src/fs/files");
+    const filesList = await opendir("src/fs/files");
+    for await (const fileName of filesList) {
+      console.log(fileName.name);
+    }
+  } catch (err) {
+    console.log(err.message);
+    console.log("FS operation failed");
+  }
 };
 
 await list();
