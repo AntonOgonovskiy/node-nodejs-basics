e3d31cb1a228d2bf4bfe00dde5bd0aef4ef9634b
Anton <antonogonovskiy@gmail.com>
2023-06-17 16:34:27 +0300

    feat: implement read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import { readFile } from "node:fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const content = await readFile("src/fs/files/fileToRead.txt", {
+      encoding: "utf8",
+    });
+    console.log(content);
+  } catch (err) {
+    console.log(err.message);
+    console.log("FS operation failed");
+  }
 };
 
 await read();
