4d1b52cf5f601af7a4adcae1035ee303a5ea1a2b
Anton <antonogonovskiy@gmail.com>
2023-06-17 19:12:39 +0300

    feat: implement args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  const arr = [];
+  process.argv.slice(2).map((key, item, array) => {
+    if (item % 2 === 0) {
+      arr.push(`${key.slice(2)} is ${array[item + 1]}`);
+    }
+  });
+  console.log(arr.join(", "));
 };
 
 parseArgs();
