ad4acb9ec17bad3940d16e3e8d96036127b568c2
Anton <antonogonovskiy@gmail.com>
2023-06-18 23:24:26 +0300

    feat: implement compress & decompress tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { rm } from "fs/promises";
+
 const compress = async () => {
-    // Write your code here 
+  const zip = createGzip();
+  const source = createReadStream("src/zip/files/fileToCompress.txt");
+  const destination = createWriteStream("src/zip/files/archive.gz");
+  pipeline(source, zip, destination);
+  rm("src/zip/files/fileToCompress.txt");
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { rm } from "fs/promises";
+
 const decompress = async () => {
-    // Write your code here 
+  const zip = createGunzip();
+  const source = createReadStream("src/zip/files/archive.gz");
+  const destination = createWriteStream("src/zip/files/fileToCompress.txt");
+  pipeline(source, zip, destination);
+  rm("src/zip/files/archive.gz");
 };
 
 await decompress();
