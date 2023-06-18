7c2c6d8bf6df8df61120c827d14da0415efd265d
Anton <antonogonovskiy@gmail.com>
2023-06-19 00:23:14 +0300

    feat: clean new brunch for pr

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,18 +1,3 @@
-import { writeFile } from "node:fs/promises";
-import { Buffer } from "node:buffer";
-import { access } from "node:fs/promises";
-
-const create = async () => {
-  const data = new Uint8Array(Buffer.from("I am fresh and young"));
-  try {
-    await access("src/fs/files/fresh.txt");
-    console.log("FS operation failed");
-  } catch (err) {
-    if (err) {
-      const promise = writeFile("src/fs/files/fresh.txt", data);
-      await promise;
-    }
-  }
-};
+const create = async () => {};
 
 await create();
